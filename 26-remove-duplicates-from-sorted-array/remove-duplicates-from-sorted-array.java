ba0f8734832d0d361295ab715156b8012985c301
Ritu Koley <100666808+RituKoley@users.noreply.github.com>
2025-02-21 23:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (12.42%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,13 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int lastUniqueIndex=0;
-        for(int i=1; i<nums.length; i++){
-            if(nums[lastUniqueIndex] != nums[i]){
-                nums[lastUniqueIndex+1] = nums[i];
-                lastUniqueIndex++;
-            }
+        int k=0;  //lastUniqueIndex
+        for(int i=1; i<nums.length; i++){ //_,1,2
+            if(nums[k] != nums[i]){  //1 == 1 false, 1 == 2
+                nums[k+1] = nums[i]; //,        nums[1]=2
+                k++;                //1
+            }//nums[0]=1, nums[1]=2,  _
         }
-        return lastUniqueIndex+1;
+        return k+1;  //1+1=2 , 0 1 2, 1,2,_
     }
 }
 // TC = O(n) SC = O(1)
